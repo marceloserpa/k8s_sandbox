73bcd77ba6233728528d86868df66fa446df9b13
marceloserpa <jdev.marcelo@gmail.com>
2021-11-14 13:58:32 -0300

    implement leaderboard using redis

--- mini-k8s-nodeapp/index.js
+++ mini-k8s-nodeapp/index.js
@@ -1,12 +1,48 @@
 
+const redis = require("redis");
+
+
+const redishost = process.env.REDIS_HOST
+
+//redis[s]://[[username][:password]@][host][:port][/db-number]
+const redisUrl = 'redis://@'+redishost
+
+console.log(`creating connection with ${redisUrl}`)
+const redisClient = redis.createClient({
+  url: redisUrl
+});
+
+
+redisClient.connect()
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+app.get('/leaderboard/games/:gameId/users/:userId/score/:score', async (req, res) => {
+
+  const gameId = req.params.gameId
+  const userId = req.params.userId
+  const score = req.params.score
+
+  const key = `game:${gameId}`
+  const member = `user:${userId}`
+
+  await redisClient.zIncrBy(key, score, userId, member)
+
+
+  res.send('Game ' + gameId + ' user ' + userId + ' score ' + score)
+})
+
+app.get('/leaderboard/games/:gameId', async (req, res) => {
+  const gameId = req.params.gameId
+  const key = `game:${gameId}`
+  const scoresRedis = await redisClient.sendCommand(['zrevrange', key, '0', '-1', 'withscores']);
+
+
+  res.send(scoresRedis)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at ... http://localhost:${port}`)
 })
