f68f20f6ccc64c3ba096787e8a4909799c764fbe
marceloserpa <jdev.marcelo@gmail.com>
2023-01-25 22:57:46 -0300

    express + mysql

---
+++ express-mysql/index.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+    host     : '172.22.0.2',
+    user     : 'root',
+    password : 'mypassword'
+    });
+
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+    });
+
+    connection.end();
+
+
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
