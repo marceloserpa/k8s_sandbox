248b2e3b1155b06dc81a92d94f85e0989bfcac69
marceloserpa <jdev.marcelo@gmail.com>
2023-01-28 14:22:40 -0300

    having fun with k8s and express

--- express-mysql/index.js
+++ express-mysql/index.js
@@ -3,26 +3,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/ping', (req, res) => {
 
-    var mysql      = require('mysql');
-    var connection = mysql.createConnection({
-    host     : 'mysql:3306',
-    user     : 'root',
-    password : 'mypassword'
+    const mysql = require('mysql');
+    const host = 'mysql-service';
+    const user = 'root';
+    const password = 'test1234';
+
+    console.log(`Trying to connect with: host: ${host} user: ${user} password: ${password} `);
+
+
+    const connection = mysql.createConnection({
+      host     : host,
+      user     : user,
+      password : password
     });
 
     connection.connect();
 
     connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].solution);
+    if (err) {
+      //throw err;
+      res.send("Error" + err)
+    };
+      const result = "The solution is: " + rows[0].solution;
+      res.send(result)
     });
 
     connection.end();
 
 
-    res.send('Hello World!')
+    
 })
 
 app.listen(port, () => {
