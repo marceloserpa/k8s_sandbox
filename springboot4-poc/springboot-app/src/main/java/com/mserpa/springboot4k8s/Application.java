bc9fc647e299616513b73394504cdecca5dc5ca3
marceloserpa <jdev.marcelo@gmail.com>
2025-12-04 19:27:48 -0300

    SpringBoot4 + docker

---
+++ springboot4-poc/springboot-app/src/main/java/com/mserpa/springboot4k8s/Application.java
@@ -0,0 +1,13 @@
+package com.mserpa.springboot4k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ springboot4-poc/springboot-app/src/main/java/com/mserpa/springboot4k8s/HelloController.java
@@ -0,0 +1,14 @@
+package com.mserpa.springboot4k8s;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "world";
+    }
+
+}
